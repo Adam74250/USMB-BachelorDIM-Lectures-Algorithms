176df5a270921af48b8ec69f8945aa24db417138
Adam MACHEDA <machedaa@iut-acy.local>
2017-09-07 16:24:34 +0200

    Exercice 4 # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up-to-date with 'origin/master'. # Changes to be committed: # # 	modified:   assignments/Session1/S1_algotools.py

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -6,9 +6,12 @@ Session 1
 import numpy
 
 tab = [2, 4, -1, 0, -1, 2]
-mat = numpy.zeros([3, 4])
+mat = numpy.zeros([7, 8])
 mat[1, 3] = 1
+mat[2:7, 3:6] = 1
+mat[6:7, 3:8] = 1
 
+##
 # This function computes the average of the positive elements of a table
 def average_above_zero(tab):
     if len(tab) == 0 :
@@ -25,6 +28,7 @@ def average_above_zero(tab):
 
         return float(Moy)
 
+##
 # This function gets the maximum value of a table
 def max_value(tab):
     if len(tab) == 0 :
@@ -37,6 +41,7 @@ def max_value(tab):
 
         return float(Max)
 
+##
 # This function reverses a table without the use of any other table
 def reverse_table(tab):
     n = len(tab)
@@ -51,6 +56,8 @@ def reverse_table(tab):
 
         return tab
 
+##
+# Selective sorting
 def selective_sort(tab):
     n = len(tab)
     for i in range(n):
@@ -66,14 +73,16 @@ def selective_sort(tab):
 
     return tab
 
+##
+# Bubble sorting
 def bubble_sort(tab):
     n = len(tab)
     tourEnCours = 0
     traitement = True
 
-    while traitement == True :
+    while traitement == True:
         traitement = False
-        for i in range (0, n-tourEnCours-1) :
+        for i in range (0, n-tourEnCours-1):
             if tab[i] > tab[i+1]:
                 valEnCours = tab[i]
                 tab[i] = tab[i+1]
@@ -84,4 +93,28 @@ def bubble_sort(tab):
 
     return tab
 
-print("{var}".format(var=bubble_sort(tab)))
+##
+# Bounding box
+def roi_bbox(mat):
+    xmin = mat.shape[0]
+    xmax = 0
+    ymin = mat.shape[1]
+    ymax = 0
+
+    for i in range(mat.shape[0]):
+        for j in range(mat.shape[1]):
+            if mat[i, j] > 0:
+                if xmin > i:
+                    xmin = i
+                if xmax < i:
+                    xmax = i
+
+                if ymin > j:
+                    ymin = j
+                if ymax < j:
+                    ymax = j
+
+    return [[ymin, xmin], [ymax, xmin], [ymin, xmax], [ymax, xmax]]
+
+print(mat)
+print("{var}".format(var=roi_bbox(mat)))
