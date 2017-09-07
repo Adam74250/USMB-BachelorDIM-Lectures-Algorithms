d33f68380dbdf0b903a0204fa28c328e8a56f84b
Adam MACHEDA <machedaa@iut-acy.local>
2017-09-07 13:33:18 +0200

    Exercice 3 # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up-to-date with 'origin/master'. # Changes to be committed: # # 	modified:   assignments/Session1/S1_algotools.py

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -3,20 +3,25 @@ Session 1
 @author : Adam MACHEDA, LP Dim
 """
 
-tab = [2, 4, -1]
+tab = [2, 4, -1, 0, -1, 2]
 
+# This function computes the average of the positive elements of a table
 def algo1(tab):
-    Som = 0
-    N = 0
+    if len(tab) == 0 :
+        return ValueError('Valeur nulle')
+    else :
+        Som = 0
+        N = 0
 
-    for item in tab:
-        if item > 0:
-            Som += item
-            N += 1
-    Moy = float(Som)/float(N)
+        for item in tab:
+            if item > 0:
+                Som += item
+                N += 1
+        Moy = float(Som)/float(N)
 
-    return float(Moy)
+        return float(Moy)
 
+# This function gets the maximum value of a table
 def algo2(tab):
     if len(tab) == 0 :
         return ValueError('Valeur nulle')
@@ -28,4 +33,17 @@ def algo2(tab):
 
         return float(Max)
 
-print("Insertion balistique : {var}".format(var=algo2(tab)))
+# This function reverses a table without the use of any other table
+def algo3(tab):
+    if len(tab) == 0 :
+        return ValueError('Valeur nulle')
+    else :
+        for i in range(len(tab)) :
+            if i < len(tab)/2:
+                valEnCours = tab[i]
+                tab[i] = tab[(len(tab)-i)-1]
+                tab[(len(tab)-i)-1] = valEnCours
+
+        return tab
+
+print("{var}".format(var=algo3(tab)))
